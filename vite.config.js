661f5fc1211b4e08649bde044e223a4619f4eb8f
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-02 21:34:35 +0545

    env file managed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // plugins: [tailwindcss()],
+  define: {
+    //env variables from .env file
+    "process.env.REACT_APP_TMDB_KEY": JSON.stringify(
+      process.env.REACT_APP_TMDB_KEY
+    ),
+  },
 });
