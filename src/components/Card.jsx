e09dd1784c065fc2f39b37cd22b67df21c29f371
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-04 20:39:57 +0545

    fixed small bugs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { IMG_CDN_URL } from "../utils/constants";
 import { Link } from "react-router";
 
-const Card = ({ posterPath, title, rating, releaseDate, id }) => {
+const Card = ({
+  posterPath,
+  title,
+  rating,
+  releaseDate,
+  id,
+  addFavourite,
+  isMovieFavourite,
+}) => {
+  const isFav = isMovieFavourite.some((fav) => fav.id === id);
   return (
     <div className="w-60 p-2 bg-white rounded-lg shadow-md transition-transform transform hover:scale-105 hover:shadow-lg">
       <img
@@ -16,6 +25,12 @@ const Card = ({ posterPath, title, rating, releaseDate, id }) => {
         <p className="text-gray-500">{releaseDate}</p>
         <button className="px-4 py-2 bg-blue-400 text-white font-semibold rounded-lg shadow-md hover:bg-blue-200 transition duration-300">
           <Link to={`/movieDetails/${id}`}>Details</Link>
+        </button>
+        <button
+          onClick={addFavourite}
+          className="text-2xl transition-transform transform hover:scale-125"
+        >
+          {isFav ? "💖" : "❤️"}
         </button>
       </div>
     </div>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies } from "../redux/movieSlice";
@@ -8,18 +8,41 @@ const Movies = () => {
   const movies = useSelector((state) => state.movies.movieCards);
   const searchQuery = useSelector((state) => state.movies.searchQuery);
 
+  const [favourites, setFavourites] = useState([]);
+
   useEffect(() => {
-    // searchMovie();
     dispatch(fetchMovies(searchQuery));
   }, [searchQuery]);
+
+  useEffect(() => {
+    const storedFavourites =
+      JSON.parse(localStorage.getItem("favourites")) || [];
+    setFavourites(storedFavourites);
+  }, []);
+
+  const handleFavourite = (movie) => {
+    let updatedFavourites;
+    const isMovieFavourite = favourites.some((fav) => fav.id === movie.id);
+
+    if (isMovieFavourite) {
+      updatedFavourites = favourites.filter((fav) => fav.id !== movie.id);
+    } else {
+      updatedFavourites = [...favourites, movie];
+    }
+
+    setFavourites(updatedFavourites);
+    localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
+  };
 
   return (
     <>
       <h1 className="text-3xl font-bold text-center p-2">All MOVIES</h1>
       <div className="flex flex-wrap gap-10 justify-center mt-5">
         {movies.length > 0 ? (
-          movies?.map((movie) => (
+          movies.map((movie) => (
             <Card
+              isMovieFavourite={favourites}
+              addFavourite={() => handleFavourite(movie)}
               key={movie.id}
               id={movie.id}
               posterPath={movie.poster_path}
