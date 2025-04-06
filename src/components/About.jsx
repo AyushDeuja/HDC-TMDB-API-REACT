4a6abcb834c882cc478da62869473e09ed33c917
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-06 11:47:44 +0545

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-center p-2">About</h1>
+      <h1 className="text-3xl font-bold text-center p-2">This Is About Page</h1>
     </div>
   );
 };
