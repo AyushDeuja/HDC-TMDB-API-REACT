4eaf5e44c498b9616e537787bd76ce16788c657a
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-19 09:05:07 +0545

    errors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-// import Movies from "./components/Movies";
-// import NavBar from "./components/NavBar";
-// import Trending from "./components/Trending";
-// import MovieDetails from "./components/MovieDetails";
+import Movies from "./components/Movies";
+import NavBar from "./components/NavBar";
+import Trending from "./components/Trending";
+import MovieDetails from "./components/MovieDetails";
 import { Provider } from "react-redux";
-// import About from "./components/About";
+import About from "./components/About";
 import store from "./redux/store";
-// import FavouriteMovies from "./components/FavouriteMovies";
+import FavouriteMovies from "./components/FavouriteMovies";
 import ThemeToggler from "./components/ThemeToggler";
-import { lazy, Suspense } from "react";
+import { Suspense } from "react";
 
 // lazy loading the components
 // This is a performance optimization technique that allows you to load components only when they are needed, rather than loading them all at once when the app starts. This can help reduce the initial load time of your app and improve its overall performance.
-const NavBar = lazy(() => import("./components/Card"));
-const Trending = lazy(() => import("./components/Trending"));
-const MovieDetails = lazy(() => import("./components/MovieDetails"));
-const About = lazy(() => import("./components/About"));
-const FavouriteMovies = lazy(() => import("./components/FavouriteMovies"));
-const Movies = lazy(() => import("./components/Movies"));
+// const NavBar = lazy(() => import("./components/Card"));
+// const Trending = lazy(() => import("./components/Trending"));
+// const MovieDetails = lazy(() => import("./components/MovieDetails"));
+// const About = lazy(() => import("./components/About"));
+// const FavouriteMovies = lazy(() => import("./components/FavouriteMovies"));
+// const Movies = lazy(() => import("./components/Movies"));
 
 function AppLayout() {
   return (
