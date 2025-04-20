f329a1e0886fb1efe6706e2e1fc3b6a3aeaf99f9
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-20 20:26:00 +0545

    login logout in favourites

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,15 +1,23 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 const AuthContext = createContext();
 
-const AuthProvier = ({ children }) => {
+const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  }, []);
+
   const login = (username, password) => {
     if (username === "admin" && password === "admin") {
-      setUser({ username });
+      setUser(username);
+      localStorage.setItem("user", JSON.stringify(username));
       navigate("/favourites");
     } else {
       alert("Invalid credentials");
@@ -20,11 +28,13 @@ const AuthProvier = ({ children }) => {
     setUser(null);
     navigate("/login");
   };
+
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
 export const useAuth = () => useContext(AuthContext);
-export default AuthProvier;
+export default AuthProvider;
--- src/components/FavouriteMovies.jsx
+++ src/components/FavouriteMovies.jsx
@@ -1,16 +1,23 @@
-import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Card from "./Card";
+import { useAuth } from "../auth/AuthContext";
 
 const FavouriteMovies = () => {
   const favouriteMovies = useSelector((state) => state.movies.favouriteMovies);
+  const { logout } = useAuth();
 
-  useEffect(() => {
-    localStorage.setItem("favouriteMovies", JSON.stringify(favouriteMovies));
-  }, [favouriteMovies]);
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center p-2">Favourite Movies</h1>
+    <div className="p-4">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-3xl font-bold text-center">Favourite Movies</h1>
+        <button
+          onClick={logout}
+          className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow transition"
+        >
+          Logout
+        </button>
+      </div>
+
       <div className="flex flex-wrap gap-10 justify-center mt-5">
         {favouriteMovies.length > 0 ? (
           favouriteMovies.map((movie) => <Card key={movie.id} movie={movie} />)
