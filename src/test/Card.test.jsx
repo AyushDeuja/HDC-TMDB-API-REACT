8bd9074a2bc206ee05b26dee1982bac4a7542bae
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-22 08:54:43 +0545

    test case added

--- src/test/Card.test.jsx
+++ src/test/Card.test.jsx
@@ -1,5 +1,13 @@
-import { render } from "@testing-library/react";
+import { getByText, render } from "@testing-library/react";
+import Card from "../components/Card";
+
+const mockMovie = {
+  id: 1,
+  content: "This is a khatra movie",
+  title: "Inception",
+};
 
 test("movie card rendering", () => {
-  render;
+  render(<Card title={mockMovie.title} />);
+  expect(screen.getByText("Inception").tobeinTheDocument());
 });
