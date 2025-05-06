353be201ddd9acaefa105a7293f0171735634c1d
AyushDeuja <ayushdeuja11@gmail.com>
2025-05-06 20:12:22 +0545

    removed unwanted consoles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,6 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
     login(userName, password);
   };
 
